dd08c2be9df10630bc51823c62b22bf287875662
Luca Sepe <luca.sepe@gmail.com>
2025-05-07 09:46:43 +0200

    feat: refactor e2e signup helper function to handle jwt

--- e2e/e2e.go
+++ e2e/e2e.go
@@ -9,6 +9,7 @@ import (
 
 	xcontext "github.com/krateoplatformops/plumbing/context"
 	"github.com/krateoplatformops/plumbing/env"
+	"github.com/krateoplatformops/plumbing/jwtutil"
 	"github.com/krateoplatformops/plumbing/kubeconfig"
 	"github.com/krateoplatformops/plumbing/signup"
 	"github.com/krateoplatformops/plumbing/slogs/pretty"
@@ -46,8 +47,30 @@ func Logger(traceId string) types.StepFunc {
 	}
 }
 
-func SignUp(user string, groups []string, namespace string) types.StepFunc {
+type SignUpOptions struct {
+	Username   string
+	Groups     []string
+	JWTSignKey string
+	Namespace  string
+	Duration   time.Duration
+}
+
+func SignUp(opts SignUpOptions) types.StepFunc {
+	if opts.Duration == 0 {
+		opts.Duration = time.Minute * 15
+	}
+
 	return func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		accessToken, err := jwtutil.CreateToken(jwtutil.CreateTokenOptions{
+			Username:   opts.Username,
+			Groups:     opts.Groups,
+			SigningKey: opts.JWTSignKey,
+			Duration:   opts.Duration,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
 		dat, err := os.ReadFile(cfg.KubeconfigFile())
 		if err != nil {
 			t.Fatal(err)
@@ -59,18 +82,25 @@ func SignUp(user string, groups []string, namespace string) types.StepFunc {
 		}
 
 		ep, err := signup.Do(context.TODO(), signup.Options{
+			RestConfig:   cfg.Client().RESTConfig(),
 			CAData:       in.Clusters[0].Cluster.CertificateAuthorityData,
 			ServerURL:    in.Clusters[0].Cluster.Server, //"https://kubernetes.default.svc",
-			CertDuration: time.Minute * 30,
-			RestConfig:   cfg.Client().RESTConfig(),
-			Namespace:    namespace,
-			Username:     user,
-			UserGroups:   groups,
+			CertDuration: opts.Duration,
+			Namespace:    opts.Namespace,
+			Username:     opts.Username,
+			UserGroups:   opts.Groups,
 		})
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		return xcontext.BuildContext(ctx, xcontext.WithUserConfig(ep))
+		return xcontext.BuildContext(ctx,
+			xcontext.WithAccessToken(accessToken),
+			xcontext.WithUserInfo(jwtutil.UserInfo{
+				Username: opts.Username,
+				Groups:   opts.Groups,
+			}),
+			xcontext.WithUserConfig(ep),
+		)
 	}
 }
